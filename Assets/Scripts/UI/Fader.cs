52899fe2ec7b1d92b1a3294d002d8a3bca20d728
learner-d <34832600+learner-d@users.noreply.github.com>
2021-02-08 19:14:28 +0200

    MainUi redesign

--- Assets/Scripts/UI/Fader.cs
+++ Assets/Scripts/UI/Fader.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 
 namespace PhysRehab.UI
 {
+    [RequireComponent(typeof(Image))]
+    [ExecuteInEditMode]
     public class Fader : MonoBehaviour
     {
         public static Fader Instance { get; private set; }
+        [SerializeField]
+        [Range(0, 1)]
+        private float _fading;
+
         private Canvas _canvas;
         private Image _faderImg;
+
+        public bool IsVisible { get; set; } = true;
+
         private void Awake()
         {
             _canvas = GetComponent<Canvas>();
@@ -18,18 +27,27 @@ namespace PhysRehab.UI
             _faderImg = GetComponent<Image>();
             if (_faderImg == null)
                 _faderImg = gameObject.AddComponent<Image>(); 
+            IsVisible = false;
             Instance = this;
-            Fading(0);
         }
 
-        public void Fading(float amount)
+        private void UpdateFading(float amount)
         {
             if (amount < 0 || amount > 1)
                 throw new System.ArgumentOutOfRangeException(nameof(amount));
 
-            _faderImg.enabled = Math.Abs(amount) > float.Epsilon;
+            _faderImg.enabled = IsVisible && Math.Abs(amount) > float.Epsilon;
             Color oldColor = _faderImg.color;
             _faderImg.color = new Color(oldColor.r, oldColor.g, oldColor.b, amount);
+        }
+
+        private long _framesCount = 0;
+        private void Update()
+        {
+            if (_framesCount++ % 10 == 0 || Application.isPlaying == false)
+            {
+                UpdateFading(_fading); 
+            }
         }
     } 
 }
