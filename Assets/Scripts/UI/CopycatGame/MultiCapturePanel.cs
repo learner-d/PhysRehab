3d1cd38be35816ca0c655e3d8a3f3dd16a1e4805
learner-d <34832600+learner-d@users.noreply.github.com>
2021-02-11 15:08:12 +0200

    Ogon!!!

--- Assets/Scripts/UI/CopycatGame/MultiCapturePanel.cs
+++ Assets/Scripts/UI/CopycatGame/MultiCapturePanel.cs
@@ -2,6 +2,7 @@ using PhysRehab.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class MultiCapturePanel : DialogPanelBase
@@ -18,11 +19,29 @@ public class MultiCapturePanel : DialogPanelBase
     public int CapturingCount { get; private set; } = 0;
     public bool DataAcquired { get; private set; } = false;
 
+    public event UnityAction DataAquiredEvent;
 
     private bool AcquireData()
     {
-        
-        return false;
+        if (string.IsNullOrEmpty(_capturingName_infld.text))
+            return false;
+        CapturingName = _capturingName_infld.text;
+
+        if (string.IsNullOrEmpty(_capturingInterval_infld.text) || !float.TryParse(_capturingInterval_infld.text, out float parsedInterval)
+            || float.IsNaN(parsedInterval)
+            || parsedInterval <= 0 || parsedInterval >= 1000)
+            return false;
+        CapturingInterval = parsedInterval;
+
+        if (string.IsNullOrEmpty(_capturingCount_infld.text) || !int.TryParse(_capturingCount_infld.text, out int parsedCount)
+            || parsedCount <= 0)
+            return false;
+        CapturingCount = parsedCount;
+
+        if (_visible)
+            DataAquiredEvent?.Invoke();
+
+        return true;
     }
 
     public void _Btn_StartCaption_Click()
